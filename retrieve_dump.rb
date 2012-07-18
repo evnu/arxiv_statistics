f386df3835106bf2a0beabe56b076237aaa4c26e
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2012-07-18 14:45:49 +0200

    dump when finished

--- retrieve_dump.rb
+++ retrieve_dump.rb
@@ -8,15 +8,25 @@ require 'ripl'
 
 client = OAI::Client.new 'http://export.arxiv.org/oai2'
 
-resumption_token = nil
+resumption_token = :first_run
 
-all_responses = []
+@all_records = []
+@file = "dump.bin"
+
+def dump_data
+    puts "Dumping to #{@file}"
+    serialized = Marshal.dump(@all_records)
+    File.open(@file, "w+"){|f| f.write(serialized)}
+end
+
+# dump on Ctrl+C
+trap("INT"){dump_data() and exit}
 
 begin
     # construct a query
     # if this is the first round, use a date
     # if this isn't the first round, set the resumption_token
-    request = if resumption_token
+    request = if resumption_token and resumption_token != :first_run
                   {:resumption_token => resumption_token}
               else
                   {:from => "1991-01-01",
@@ -36,10 +46,35 @@ begin
         puts "Forced to sleep #{seconds} s"
         sleep(Integer(seconds))
     else
+        if response.resumption_token.nil?
+            Ripl.start :binding => binding
+            exit
+        end
         resumption_token = response.resumption_token
         puts "#{resumption_token}"
-        all_responses << response
+
+        puts "Filtering record information"
+        current = response.map do |record|
+            meta = record.metadata.children[1]
+
+            id = meta.elements["id"].text
+            versions = meta.children.map do |child|
+                next if child.class != REXML::Element
+                {
+                    :version => child.attributes["version"],
+                    :date => child.children[0].text,
+                    :size => child.children[1].text
+                } if child.name == "version"
+            end.compact!
+
+            {id => versions}
+        end
+
+        @all_records << current
     end
 end while resumption_token
 
+dump_data
+
+puts "Finished retrieving data"
 Ripl.start :binding => binding
